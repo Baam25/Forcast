49be1c6a9f1587b8e1e16caa719e328c6d332ca6
Harish Gonnabattula <CarpeDiem@Harishs-MacBook-Pro-2.local>
2017-06-22 00:20:37 +0530

    initial commit with UI design

---
+++ Forcast/ForecastWeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  ForecastWeatherCell.swift
+//  Forcast
+//
+//  Created by Harish Gonnabhaktula on 22/06/17.
+//  Copyright © 2017 Harish Gonnabattula. All rights reserved.
+//
+
+import UIKit
+
+class ForecastWeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Forcast/HomeScreenController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeScreenController.swift
+//  Forcast
+//
+//  Created by Harish Gonnabhaktula on 22/06/17.
+//  Copyright © 2017 Harish Gonnabattula. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenController: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    @IBAction func toggleWeather(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func shareAction(_ sender: UIBarButtonItem) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
